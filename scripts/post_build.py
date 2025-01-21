8b9424e96f3874892b658daf79ea1c059b7e160b
Vladyslav Hnatiuk <aders1234@gmail.com>
2025-01-21 10:32:17 +0100

    Fix precompilation

--- scripts/post_build.py
+++ scripts/post_build.py
@@ -1,21 +1,15 @@
 import shutil
 import sys
-import site
 from pathlib import Path
 
 import mypy
 
+
 def post_build():
     output_dir = Path(__file__).parent.parent / 'main.dist'
     mypyc_id = "8357f96abe52477c085b__mypyc"
-    for sitepackage_path in site.getsitepackages():
-        # black
-        try:
-            mypyc_file_str = next(p for p in Path(sitepackage_path).glob(f'{mypyc_id}.*.*'))
-        except StopIteration:
-            raise Exception("mypy lib not found")
 
-    lib_path = Path(mypyc_file_str)
+    lib_path = list(Path(__file__).parent.parent.glob(f'{mypyc_id}.*.*'))[0]
     shutil.copyfile(lib_path, output_dir / lib_path.name)
 
 
